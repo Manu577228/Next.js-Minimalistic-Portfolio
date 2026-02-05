4feff8bdb101064746dca0919ce068bf564112d0
Manu Bharadwaj <102406248+Manu577228@users.noreply.github.com>
2026-02-05 18:00:44 +0530

    Update LandingPage.tsx

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 const titles = [
   'Software Engineer',
   <span>
-    Springboot & <br />
+    Springboot, <br />
     Django & <br />
     React / Angular
   </span>,
